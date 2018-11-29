ddcdec8279a78897bc1d76e82f9814e0a964604b
weigun <weigun@weigun.com>
2018-11-29 14:48:20 +0800

    delete old files

--- seeder/celllink.h
+++
@@ -1,8 +0,0 @@
-#ifndef CELLLINK_H
-#define CELLLINK_H
-
-#include "protocol.h"
-
-bool TestNode(const CellService &cip, int &ban, int &client, std::string &clientSV, int &blocks, std::vector<CellAddress>* vAddr);
-
-#endif
