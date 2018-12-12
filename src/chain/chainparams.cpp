4fe988b326ada1f9e088efbd6216f5bbaebf5df2
scott <378282715@qq.com>
2018-12-12 15:44:30 +0800

    添加checkpoint2 ，特定高度产生的block hash只能是config hash

--- src/chain/chainparams.cpp
+++ src/chain/chainparams.cpp
@@ -101,6 +101,16 @@ int MCChainParams::GetDefaultPort() const
     }
 }
 
+const uint256* MCChainParams::GetCheckpointHeightHash(int height) const
+{
+    MapCheckpoints::const_iterator cit = checkpointData.mapCheckpoints.find(height);
+    if (cit != checkpointData.mapCheckpoints.end())
+    {
+        return &cit->second;
+    }
+    return nullptr;
+}
+
 void MCChainParams::UpdateVersionBitsParameters(Consensus::DeploymentPos d, int64_t nStartTime, int64_t nTimeout)
 {
     consensus.vDeployments[d].nStartTime = nStartTime;
@@ -225,9 +235,9 @@ public:
         fRequireStandard = true;
         fMineBlocksOnDemand = false;
 
-        //checkpointData = (MCCheckpointData) {
-        //    {
-        //        // { 11111, uint256S("0x0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d")},
+        checkpointData = {
+            {
+             //    { 1000, uint256S("0xe7bb1cc7e1d083f1a2c2cb1ff00e943f50ed43d9184676f62679a30855c2098d")},
         //        // { 33333, uint256S("0x000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0a6")},
         //        // { 74000, uint256S("0x0000000000573993a3c9e41ce34471c079dcf5f52a0e824a81e7f953b8661a20")},
         //        // {105000, uint256S("0x00000000000291ce28027faea320c8d2b054b2e0fe44a773f3eefb151d6bdc97")},
@@ -240,8 +250,8 @@ public:
         //        // {250000, uint256S("0x000000000000003887df1f29024b06fc2200b55f8af8f35453d7be294df2d214")},
         //        // {279000, uint256S("0x0000000000000001ae8c72a0b0c301f67e3afca10e819efa9041e458e9bd7e40")},
         //        // {295000, uint256S("0x00000000000000004d9b4ef50f0f9d686fd69db2e03af35a100370c64632a983")},
-        //    }
-        //};
+            }
+        };
 
         chainTxData = ChainTxData{
             // Data as of block 000000000000000000d97e53664d17967bd4ee50b23abb92e54a34eb222d15ae (height 478913).
