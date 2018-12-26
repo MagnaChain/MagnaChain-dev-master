6821674e9d8f8607f243c84cbbb2b21509ecc544
scott <378282715@qq.com>
2018-12-26 20:41:45 +0800

    base58 test data .h

--- src/test/base58_tests.cpp
+++ src/test/base58_tests.cpp
@@ -125,7 +125,7 @@ public:
 // Goal: check that parsed keys match test payload
 BOOST_AUTO_TEST_CASE(base58_keys_valid_parse)
 {
-    UniValue tests = read_json(std::string(json_tests::base58_keys_valid, json_tests::base58_keys_valid + sizeof(json_tests::base58_keys_valid)));
+    UniValue tests = read_json(std::string(json_tests::base58_keys_valid));
     MagnaChainSecret secret;
     MagnaChainAddress addr;
 	SELECTPARAMS(MCBaseChainParams::MAIN);
@@ -182,7 +182,7 @@ BOOST_AUTO_TEST_CASE(base58_keys_valid_parse)
 // Goal: check that generated keys match test vectors
 BOOST_AUTO_TEST_CASE(base58_keys_valid_gen)
 {
-    UniValue tests = read_json(std::string(json_tests::base58_keys_valid, json_tests::base58_keys_valid + sizeof(json_tests::base58_keys_valid)));
+    UniValue tests = read_json(std::string(json_tests::base58_keys_valid));
 
     for (unsigned int idx = 0; idx < tests.size(); idx++) {
         UniValue test = tests[idx];
@@ -249,7 +249,7 @@ BOOST_AUTO_TEST_CASE(base58_keys_valid_gen)
 // Goal: check that base58 parsing code is robust against a variety of corrupted data
 BOOST_AUTO_TEST_CASE(base58_keys_invalid)
 {
-    UniValue tests = read_json(std::string(json_tests::base58_keys_invalid, json_tests::base58_keys_invalid + sizeof(json_tests::base58_keys_invalid))); // Negative testcases
+    UniValue tests = read_json(std::string(json_tests::base58_keys_invalid)); // Negative testcases
     MagnaChainSecret secret;
     MagnaChainAddress addr;
 
