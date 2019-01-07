78355ae7f815c8f754cd205b88d2f8efaf6a13da
ray <417241656@qq.com>
2019-01-07 11:18:16 +0800

    调整基础费率

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -47,7 +47,7 @@ static const unsigned int DEFAULT_KEYPOOL_SIZE = 1000;
 //! -paytxfee default
 static const MCAmount DEFAULT_TRANSACTION_FEE = 0;
 //! -fallbackfee default
-static const MCAmount DEFAULT_FALLBACK_FEE = 150000000;
+static const MCAmount DEFAULT_FALLBACK_FEE = 30000000;
 //! -m_discard_rate default
 static const MCAmount DEFAULT_DISCARD_FEE = 75000000;
 //! -mintxfee default
