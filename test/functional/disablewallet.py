336c6833a9032e3035fe4b8c6f8c9bb91a7b4e6f
weigun <weigun@weigun.com>
2019-01-09 21:01:50 +0800

    完善disablewallet

--- test/functional/disablewallet.py
+++ test/functional/disablewallet.py
@@ -42,6 +42,7 @@ class DisableWalletTest (MagnaChainTestFramework):
 
         # Make sure wallet is really disabled then return "disablewallet option open, no address to mine"
         assert_raises_rpc_error(-25, "disablewallet option open, no address to mine", node.generatetoaddress, 1, new_address)
+
         # assert_raises_rpc_error(-5, "Invalid address", node.generatetoaddress, 1, invalid_address)
         # assert_raises_rpc_error(-5, "Invalid address", node.generatetoaddress, 1, contract_id)
 
