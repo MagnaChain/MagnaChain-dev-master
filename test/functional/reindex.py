eaa2b26bed7ade846c0f41306f9b455af9cf1b34
weigun <weigun@weigun.com>
2019-01-29 20:33:56 +0800

    ReindexTest add contract testcase

--- test/functional/reindex.py
+++ test/functional/reindex.py
@@ -10,7 +10,7 @@
 """
 
 from test_framework.test_framework import MagnaChainTestFramework
-from test_framework.util import assert_equal
+from test_framework.util import assert_equal,generate_contract
 import time
 
 class ReindexTest(MagnaChainTestFramework):
@@ -19,13 +19,25 @@ class ReindexTest(MagnaChainTestFramework):
         self.setup_clean_chain = True
         self.num_nodes = 1
 
-    def reindex(self, justchainstate=False):
+    def reindex(self, justchainstate=False,with_contract = False):
         self.nodes[0].generate(3)
+
+        if with_contract:
+            # make some contract transactions
+            cid = self.nodes[0].publishcontract(generate_contract(self.options.tmpdir))['contractaddress']
+            self.nodes[0].callcontract(True, 10, cid, self.nodes[0].getnewaddress(), 'payable')
+            for i in range(10):
+                self.nodes[0].callcontract(True,0,cid,self.nodes[0].getnewaddress(),'dustChangeTest',self.nodes[0].getnewaddress())
+        self.nodes[0].sendtoaddress(self.nodes[0].getnewaddress(),1)
+        self.nodes[0].generate(2)
+
         blockcount = self.nodes[0].getblockcount()
         self.stop_nodes()
         extra_args = [["-reindex-chainstate" if justchainstate else "-reindex", "-checkblockindex=1"]]
         self.start_nodes(extra_args)
+
         while self.nodes[0].getblockcount() < blockcount:
+            print(self.nodes[0].getblockcount() , blockcount)
             time.sleep(0.1)
         assert_equal(self.nodes[0].getblockcount(), blockcount)
         self.log.info("Success")
@@ -35,6 +47,11 @@ class ReindexTest(MagnaChainTestFramework):
         self.reindex(True)
         self.reindex(False)
         self.reindex(True)
+
+        self.reindex(False,with_contract = True)
+        self.reindex(True,with_contract = True)
+        self.reindex(False,with_contract = True)
+        self.reindex(True,with_contract = True)
 
 if __name__ == '__main__':
     ReindexTest().main()
