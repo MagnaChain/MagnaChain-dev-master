a673ebd2c57f7d044300793ab90680fa823b135b
weigun <283257958@qq.com>
2019-04-08 12:48:36 +0800

    [skip ci] wrong args

--- test/functional/dbcrash.py
+++ test/functional/dbcrash.py
@@ -78,7 +78,7 @@ class ChainstateWriteCrashTest(MagnaChainTestFramework):
         while time.time() - time_start < 240:
             try:
                 # Any of these RPC calls could throw due to node crash
-                self.log.info('start with args:',self.nodes[node_index].extra_args)
+                self.log.info('start with args:{}'.format(self.nodes[node_index].extra_args))
                 self.start_node(node_index)
                 self.nodes[node_index].waitforblock(expected_tip)
                 utxo_hash = self.nodes[node_index].gettxoutsetinfo()['hash_serialized_2']
