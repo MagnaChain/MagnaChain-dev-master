889fdf1cefbf493984d28350288b389e5a606003
ThreePang <wurui@matgz.com>
2019-04-19 14:49:45 +0800

    fix dynamic tx bug when reverttransaction

--- src/transaction/txmempool.cpp
+++ src/transaction/txmempool.cpp
@@ -1083,21 +1083,20 @@ MCMutableTransaction RevertTransaction(const MCTransaction& tx, const MCTransact
 uint256 MCTxMemPool::GetOriTxHash(const MCTransaction& tx, bool fFromMempool)
 {
     uint256 txHash = tx.GetHash();
-    if ((tx.IsBranchChainTransStep2() && tx.fromBranchId != MCBaseChainParams::MAIN && fFromMempool) ||
-        (tx.IsSmartContract() && tx.pContractData->contractCoinsOut.size() > 0)) {
-        auto it = mapFinalTx2OriTx.find(txHash);
-        if (it == mapFinalTx2OriTx.end()) {
-            uint256 oriTxHash = RevertTransaction(tx, nullptr, fFromMempool).GetHash();
-            mapFinalTx2OriTx[txHash] = oriTxHash;
-            return oriTxHash;
-        }
-        return it->second;
-    }
-    if (tx.IsBranchChainTransStep2() && tx.fromBranchId != MCBaseChainParams::MAIN && !fFromMempool){
+    if (tx.IsBranchChainTransStep2() && tx.fromBranchId != MCBaseChainParams::MAIN && !fFromMempool) {
         auto it = mapFinalTx2OriTx2.find(txHash);
         if (it == mapFinalTx2OriTx2.end()) {
             uint256 oriTxHash = RevertTransaction(tx, nullptr, fFromMempool).GetHash();
             mapFinalTx2OriTx2[txHash] = oriTxHash;
+            return oriTxHash;
+        }
+        return it->second;
+    }
+    else if (tx.IsDynamicTx()) {
+        auto it = mapFinalTx2OriTx.find(txHash);
+        if (it == mapFinalTx2OriTx.end()) {
+            uint256 oriTxHash = RevertTransaction(tx, nullptr, fFromMempool).GetHash();
+            mapFinalTx2OriTx[txHash] = oriTxHash;
             return oriTxHash;
         }
         return it->second;
